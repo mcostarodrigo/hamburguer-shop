8ddc2b9b965ae14b44ef54246ca04c5db27f7a99
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2023-01-10 16:34:16 -0300

    New Commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const { request } = require("express")
 const express = require("express")
+
+const port = 3000
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    return response.send("Hello Node")
+const novoPedido = []
+
+app.post('/novopedido', (request, response) => {
+
+    const resposta = {order,clientName,price,status} = request.body
+
+    novoPedido.push(resposta)
+
+    return response.status(201).json
 })
 
-app.listen(3000)
+app.listen(port , () => {
+    console.log(`server started on port ${port}`)
+})
+
+app.get('/novopedido', (request, response) => {
+    return response.json(novoPedido)
+})
